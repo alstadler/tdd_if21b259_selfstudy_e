15368d8060aec62957d221042da13b3a8b325f00
alstadler00 <alstadler.email@gmail.com>
2024-05-23 16:54:57 +0200

    added production code for unit test 1

--- src/kata1.js
+++ src/kata1.js
@@ -1,1 +1,11 @@
+function findSpaceship(map) {
+	let rows = map.split('\n'); // split map into rows
 
+	// loop through each row to find X
+	for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
+		let columnIndex = rows[rowIndex].indexOf('X');
+		if (columnIndex !== -1) { // 'X' found -> return coordinates
+			return [columnIndex, rows.length - 1 - rowIndex];
+		}
+	}
+}
