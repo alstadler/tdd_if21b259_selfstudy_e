94c51acac785555d21af901e7a9cd83da22c0ece
alstadler00 <alstadler.email@gmail.com>
2024-05-23 16:56:51 +0200

    added failing test 2

--- spec/kata1_spec.js
+++ spec/kata1_spec.js
@@ -2,5 +2,10 @@ describe ('alf_help', function () {
     it('should return spaceship coordinates when found', function () {
         let map = `..........\n..........\n..........\n.......X..\n...........\n..........`;
         expect(findSpaceship(map)).toEqual([7, 2]);
-    })
+    });
+
+    it('should return "Spaceship lost forever." if spaceship is not found', function () {
+        let map = `..........\n..........\n..........\n..........\n...........\n..........`;
+        expect(findSpaceship(map)).toEqual(['Spaceship lost forever.']);
+    });
 })
